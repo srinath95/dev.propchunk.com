1765e27cfae1e5a48aad9ebf541fd8aacba86e64
srinath95 <srinathprabhu95@gmail.com>
2016-06-25 20:09:32 +0530

    Updates added tawk and few changes

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,4 +1,3 @@
-
         <div class="row">
             <div class="col-lg-2 col-lg-offset-5">
                 <hr class="marginbot-50">
@@ -6,77 +5,146 @@
         </div>
 
 		<div class="container">
-	    
-    <div class="row col-lg-3 col-md-12 col-sm-12 col-xs-12 " style="margin-top:31px; margin-left:14px;">
+	    <div class="row col-lg-6">
+    <div class="row col-lg-12" >
+        <h5>Subscribe to Our NewsLetter</h5>
+        </div>
+      
+    <div class="row col-lg-8 col-md-12 col-sm-12 col-xs-12 " >
          <div class="input-group">
-                                <span class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span>
+        
+                                <span class="input-group-addon border-for-icons"><span class="glyphicon glyphicon-envelope"></span>
                                 </span>
                                 <input type="email" class="form-control" id="email" placeholder="Enter email" required="required" /></div>
                         
         </div>
-
-    <div class="row col-lg-2 col-md-12 col-sm-12 col-xs-12" style="margin-top:31px; margin-left:14px;" >
-                            <div class>
+<div class="row col-lg-2 col-md-12 col-sm-12 col-xs-12">
+  &nbsp;
+</div>
+    <div class="row col-lg-2 col-md-12 col-sm-12 col-xs-12"  >
+                            <div class="pull-left">
+            
                         <button type="submit" class="btn btn-skin " id="btnContactUs" >
                             Subscribe</button>
                     </div></div>
+
+ </div>
+
 	   <div class="row col-lg-2 ">
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
     </div>
 
 
-    	<div class="col-lg-4 ">
+    	<div class="row col-lg-4 ">
 			<div class="widget-contact">
 	
                 <address>
-                  <strong>We're on social networks</strong><br>
+                  <h5>We're on social networks</h5>
                         <ul class="company-social">
-                            <li class="social-facebook"><a href="#" target="_blank"><i class="fa fa-facebook"></i></a></li>
-                            <li class="social-twitter"><a href="#" target="_blank"><i class="fa fa-twitter"></i></a></li>
+                            <li class="social-facebook"><a href="http://facebook.com/PropChunk" target="_blank"><i class="fa fa-facebook"></i></a></li>
+                            <li class="social-twitter"><a href="http://twitter.com/PropChunk" target="_blank"><i class="fa fa-twitter"></i></a></li>
                         </ul>   
                 </address>                  
+           
 
-                <h5>Main Office</h5>
-				<address>
-				  D/9, Ansa Estate,<br>
-				  Opp. Kotak Bank & Vodafone Gallery,<br>
-				  Saki Naka, Andheri (E), Mumbai.<br>
-				  <abbr>P:</abbr> +91 9699 796629
+			</div>	
+		</div>
+ <div class="row col-lg-12">
+&nbsp;
+ </div>         
+<div class="row col-lg-2 col-md-3 col-sm-4 col-xs-12">
+    <address class="text-left;">
+                  <h5 id="text-left1">Company</h5>
+                      <ul class="list-group text-left">
+                            <li class="list-group-item le text-left" id="le" ><a href="<?php echo site_url('Home/about_us');?>">    About</a></li>
+                                                 </ul>   
+                </address>                  
 
-				</address>
+ </div>
 
-				<address>
-				  <strong>Email</strong><br>
-				  <a href="mailto:#"> hello@propchunk.com</a>
-				</address>	
-				<address>
+
+        
+<div class=" row col-lg-2 col-md-3 col-sm-4 col-xs-12">
+    <address class="text-left;">
+                  <h5 id="text-left1">Invest</h5>
+                        <ul class="list-group le text-left" >
+                            <li class="list-group-item le text-left" id="le"><a href="<?php echo site_url('Home/discover');?>">Browse Project</a></li>
+                            <li class="list-group-item le text-left" id="le"><a href="#">Investor Protection</a></li>
+                             
+                                                 </ul>   
+                </address>                  
+
+ </div>
+
+<div class="row col-lg-2 col-md-3 col-sm-4 col-xs-12">
+    <address class="text-left;">
+                  <h5 id="text-left1">Learn</h5>
+                        <ul class="list-group le text-left">
+                            <li class="list-group-item le text-left" id="le"><a href="#"> How it works</a></li>
+                            <li class="list-group-item le text-left" id="le"><a href="<?php echo site_url('Home/faq');?>" >FAQ's</a></li>
+                             <li class="list-group-item le text-left" id="le"><a href="#" target="_blank">Blog</a></li>
+                                                 </ul>   
+                </address>                  
+
+ </div>
+
+       <div class="row col-lg-2 ">
+&nbsp;    </div>
+
+    <div class="row col-lg-4 col-md-3 col-sm-4 col-xs-12">
+            <div class="widget-contact">
+       
+               <h5>Main Office</h5>
+                <address>
+                  D/9, Ansa Estate,<br>
+                  Opp. Kotak Bank & Vodafone Gallery,<br>
+                  Saki Naka, Andheri (E), Mumbai.<br>
+                  <abbr>P:</abbr> +91 9699 796629
+
+             
+                    </address>
+
+                <address>
+                  <strong>Email</strong><br>
+                  <a href="mailto:#"> hello@propchunk.com</a>
+                </address>  
+                <address>
 
                 <address>
                   <strong>2016. All Rights Reserved.<br />
     
     Chunk Technologies Pvt. Ltd.</strong>
                  
-                </address>  	
-			 
-                
-			</div>	
-		</div>
+                </address>      
+  </div>        
+    </div>
     </div>	
+		 
+        </div>
 
-		</div>
+
+
+
 	</section>
 	<!-- /Section: contact -->
-
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/576b8e08b57c0500209a68fd/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
     <!-- Core JavaScript Files -->
-    <script src="<?php echo base_url('/bootstrap/js/jquery.min.js');?>"></script>
+     <script src="<?php echo base_url('/bootstrap/js/jquery.min.js');?>"></script>
     <script src="<?php echo base_url('/bootstrap/js/bootstrap.min.js');?>"></script>
     <script src="<?php echo base_url('/bootstrap/js/jquery.easing.min.js');?>"></script>	
 	<script src="<?php echo base_url('/bootstrap/js/jquery.scrollTo.js');?>"></script>
 	<script src="<?php echo base_url('/bootstrap/js/wow.min.js');?>"></script>
     <!-- Custom Theme JavaScript -->
     <script src="<?php echo base_url('/bootstrap/js/custom.js');?>"></script>
-    <!--ADDED FOR GIF LOADING ICON -->
-    <script src="<?php echo base_url('http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js');?>"></script>
-    <script src="<?php echo base_url('http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.2/modernizr.js');?>"></script>
-</body>
-</html>
