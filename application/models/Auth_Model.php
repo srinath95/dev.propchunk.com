2bdcc1cd5ad824c5048d8cd1525c498ebd8e0bac
Javedgouri <javeda3740@gmail.com>
2016-06-29 10:36:36 +0530

    updated by javed gouri

--- application/models/Auth_Model.php
+++ application/models/Auth_Model.php
@@ -1,10 +1,6 @@
 <?php
-
 Class Auth_Model extends CI_Model {
-
-
 	public function registration_insert($data) {
-
 		// Query to insert data in database
 		$this->db->insert('RegisteredUser', $data);
 		if ($this->db->affected_rows() > 0) {
@@ -13,36 +9,28 @@ Class Auth_Model extends CI_Model {
 		return false;
 		}
 	}
-
 	
 	public function login_valid($EmailId,$Password) {
-
 		$query = $this->db->where(['EmailId'=>$EmailId,'Password'=>$Password])->get('RegisteredUser');
 		if ($query->num_rows()) {
-			return $query->row()->FisrtName;
+			return $query->row()->FirstName;
 		} else {
 			return false;
 		}
 	}
-
-
 	// Read data from database to show data in admin page
 	public function read_user_information($EmailId) {
-
 		$condition = "EmailId =" . "'" . $EmailId . "'";
 		$this->db->select('*');
 		$this->db->from('RegisteredUser');
 		$this->db->where($condition);
 		$this->db->limit(1);
 		$query = $this->db->get();
-
 		if ($query->num_rows() == 1) {
 		return $query->result();
 		} else {
 		return false;
 		}
 	}
-
 }
-
 ?>
--- application/models/Verify_Model.php
+++ application/models/Verify_Model.php
@@ -9,19 +9,31 @@ status:-pending
 Class Verify_Model extends CI_Model {
 
 	//function to check the hash for verifyication of emailid and upadte teh active level
-	public function checkhash($hash){
-		$query = $this->db->where('HashKey',$hash)->get('RegisteredUser');
-		if($query){
-			$this->db->where('HashKey',$hash);
+	public function checkhash($email,$hash){
+		$query = $this->db->where(['EmailId'=>$email,
+			'HashKey'=>$hash])->get('RegisteredUser');
+		if($query->num_rows() == 1){
 			$data = array('ActiveLevel' => 1, );
+			$this->db->where('EmailId',$email);
+			$this->db->where('HashKey',$hash);
 			$this->db->update('RegisteredUser',$data);
-			return TRUE;
+			return true;
 		}else{
 
-			return FALSE;
+			return false;
 
 		}
 	}//end of check hash function
+
+	public function checkhash1($email,$hash){
+		$query = $this->db->where(['EmailId'=>$email,
+			'HashKey'=>$hash])->get('RegisteredUser');
+		if($query->num_rows()==1){
+			return true;
+		}else{
+			return false;
+		}
+	}
 
 	//function to check the EmailId  is exist or not
 	public function checkuser($email){
@@ -33,5 +45,13 @@ Class Verify_Model extends CI_Model {
 			return false;
 		}
 	}//end of checkuser
+
+
+	public function updatehash($u_email,$u_hash){
+
+		$this->db->where('EmailId', $u_email);
+		$query=$this->db->update('RegisteredUser',['HashKey'=>$u_hash]);
+	}//end of update hash
+
 }//end of class verify_model
 ?>
